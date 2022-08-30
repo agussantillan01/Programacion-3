5c57c8773a2ce6f58186aa71c1a1cdf60c59c0ab
agussantillan01 <agusantillan01@hotmail.com>
2022-08-30 18:40:49 -0300

    Agregue un ejemplo sencillo de EXCEPCIONES (unicamente para ver el funcionamineto).

---
+++ Programacion III/primerasVentanasWF/Excepciones/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Excepciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.resultado = new System.Windows.Forms.Label();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(166, 93);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(151, 20);
+            this.num1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "+";
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(166, 195);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(151, 20);
+            this.num2.TabIndex = 2;
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(235, 387);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(19, 13);
+            this.resultado.TabIndex = 4;
+            this.resultado.Text = " = ";
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(219, 264);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(52, 23);
+            this.btnSuma.TabIndex = 3;
+            this.btnSuma.Text = "Calcula";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 450);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Button btnSuma;
+    }
+}
+
---
+++ Programacion III/primerasVentanasWF/Excepciones/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excepciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            int a, b, r;
+            try
+            {
+                a = int.Parse(num1.Text);
+                b = int.Parse(num2.Text);
+                r = a + b;
+
+                resultado.Text = "= " + r;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString()); //Una forma de mostrar el error
+                MessageBox.Show("Ingrese unicamente numero por favor....");
+
+
+            }
+            finally
+            {
+                MessageBox.Show("Finally -- Se ejecuta SIEMPRE, sin importar lo que suceda en try o catch");
+
+            }
+        }
+    }
+}
