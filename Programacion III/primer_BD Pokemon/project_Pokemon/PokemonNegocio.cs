d8daff8a795c8d654af00292c499f9b124c19937
agussantillan01 <agusantillan01@hotmail.com>
2022-08-31 14:26:19 -0300

    Hice mi primer coneccion a una base de datos - mini proyecto POKEMON

---
+++ Programacion III/primer_BD Pokemon/project_Pokemon/PokemonNegocio.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//PARA PODER CONECTARLO A UNA BASE DE DATOS: 
+using System.Data.SqlClient;
+
+
+namespace project_Pokemon
+{
+    internal class PokemonNegocio
+    {
+        public List<Pokemon> listar()
+        {
+            List<Pokemon> lista = new List<Pokemon>();
+            SqlConnection conexion = new SqlConnection(); //Se conecta a cierta conexion
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;// Al realizar la lectura, obtengo la instancia de un objeto
+
+            try
+            {
+                conexion.ConnectionString = "Server = DESKTOP-D2JTRHQ\\SQLEXPRESS; database = POKEDEX_DB; integrated security = true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "SELECT Numero, Nombre, Descripcion FROM POKEMONS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read()) //Si puede leer un registro, devuelve true y posiciona un puntero en la primera fila de la tabla de la BD
+                {
+                    Pokemon aux = new Pokemon();                
+                    aux.Numero = (int)lector["Numero"];
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+
+                    lista.Add(aux);
+
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+
+            }
+
+        }
+    }
+}
---
+++ Programacion III/primer_BD Pokemon/project_Pokemon/frmPokemon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_Pokemon
+{
+    public partial class frmPokemon : Form
+    {
+        public frmPokemon()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPokemon_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            dgvPokemons.DataSource = negocio.listar();
+
+        }
+    }
+}
